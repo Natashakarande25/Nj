185d27d5b2142cdfe3543966c4a704ddf26203fa
Natashakarande25 <148038201+Natashakarande25@users.noreply.github.com>
2025-03-07 14:32:29 +0530

    Update 4_Angular.js
    1

--- 4_Angular.js
+++ 4_Angular.js
@@ -1,3 +1,5 @@
+to do list
+
 var app = angular.module("todoApp", []);
 
 app.controller("TodoController", function ($scope) {
@@ -25,5 +27,47 @@ app.controller("TodoController", function ($scope) {
 
     $scope.viewTasks = function () {
         return $scope.tasks;
+    };
+});
+
+
+error handling app 
+
+var app = angular.module("errorHandlingApp", []);
+
+app.factory("errorInterceptor", function ($q) {
+    return {
+        responseError: function (rejection) {
+            if (rejection.status === 404) {
+                alert("Error 404: Resource Not Found!");
+            } else if (rejection.status === 500) {
+                alert("Error 500: Internal Server Error!");
+            } else {
+                alert("An Unexpected Error Occurred!");
+            }
+            return $q.reject(rejection);
+        }
+    };
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push("errorInterceptor");
+});
+
+app.controller("MainController", function ($scope, $http) {
+    $scope.data = [];
+    $scope.error = false;
+    $scope.errorMessage = "";
+
+    $scope.fetchData = function () {
+        $http.get("https://jsonplaceholder.typicode.com/posts")
+            .then(function (response) {
+                $scope.data = response.data;
+                $scope.error = false;
+            })
+            .catch(function () {
+                $scope.error = true;
+                $scope.errorMessage = "Failed to fetch data. Please try again later!";
+            });
     };
 });
